46852d5309b2ebd5b1eb4f9065bd95735a031457
agonzaga <agonzaga@bu.edu>
2018-11-06 18:38:45 -0500

    added cache functionality

--- DEMO/mainApp/twitterCall.py
+++ DEMO/mainApp/twitterCall.py
@@ -1,9 +1,44 @@
 import tweepy
+import sqlite3
+
+
+def create_table():
+    # Create table
+    c.execute('''CREATE TABLE tweets (Name text, tweets text)''')
+    return 0
+
+def insert_into_table(twitter_handle, tweets_list):
+    # Insert a row of data
+    for item in tweets_list:
+        c.execute("INSERT INTO tweets (Name, tweets) values (?, ?)", (twitter_handle, item))
+    return 0
+
+def close_db():
+    # Save (commit) the changes
+    conn.commit()
+    conn.close()
+    return 0
+
+
+def read_db():
+    c.execute('SELECT * FROM tweets')
+    print(c.fetchone())
+    return 0
+
 
 
 def get_tweets(twitter_handle, *number_tweets):
+    global conn 
+    global c
+    conn = sqlite3.connect('data.db')
+    c = conn.cursor()
+
     number_tweets = 30
     #  Keys
+    consumer_key = 'none'
+    consumer_secret = 'none'
+    access_token = 'none'
+    access_secret = 'none'
 
     # Authorization
     auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
@@ -23,5 +58,15 @@ def get_tweets(twitter_handle, *number_tweets):
     for item in tweets:
         tweets_list.append(item.text)
 
-    print(tweets_list)
+
+    # SQL
+    #create_table()
+    #insert_into_table(twitter_handle, tweets_list)
+    read_db()
+    close_db()
+
     return tweets_list
+
+
+if __name__ == '__main__':
+    get_tweets('neiltyson', 5)
